4be97960fce06bd063e7e5434471dd6557760bc2
tzvaita <tzvaita@gmail.com>
2021-06-12 21:55:33 +0200

    Create friendships model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_12_193009) do
+ActiveRecord::Schema.define(version: 2021_06_12_194622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friendships", force: :cascade do |t|
+    t.bigint "sent_to_id", null: false
+    t.bigint "sent_by_id", null: false
+    t.boolean "status", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sent_by_id"], name: "index_friendships_on_sent_by_id"
+    t.index ["sent_to_id"], name: "index_friendships_on_sent_to_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +34,6 @@ ActiveRecord::Schema.define(version: 2021_06_12_193009) do
     t.string "shortened"
   end
 
+  add_foreign_key "friendships", "users", column: "sent_by_id"
+  add_foreign_key "friendships", "users", column: "sent_to_id"
 end
