b0b178c3b0c99c7ad525d54574a9234577b6e07d
tzvaita <tzvaita@gmail.com>
2021-06-12 17:12:27 +0200

    Add scraping method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
+require 'nokogiri'
+require 'open-uri'
+
 module UsersHelper
+
+  def scrape_site
+    the_url = 'https://tenny-zvaita.tech'
+    html = URI.open(the_url)
+    doc = Nokogiri::HTML(html)
+
+    headings = doc.css('h1', 'h2', 'h3')
+    heading_arr = []
+
+    headings.each do |h|
+      hedVal = h.text
+      heading_arr << hedVal
+
+      print heading_arr
+    end
+  end
 end
