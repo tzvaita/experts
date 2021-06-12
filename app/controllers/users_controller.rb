833c6287d049fab81208d2998bc43e3891fbf97b
tzvaita <tzvaita@gmail.com>
2021-06-12 21:36:29 +0200

    Execute url shortening in controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         scrape_site(@user, @user.website)
+        short_url(@user, @user.website)
         format.html { redirect_to @user, notice: "User was successfully created." }
         format.json { render :show, status: :created, location: @user }
       else
