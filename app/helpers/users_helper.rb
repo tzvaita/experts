5df66573acee7af671b906a89b699556765884f0
tzvaita <tzvaita@gmail.com>
2021-06-13 20:07:24 +0200

    Create logic for introductions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -26,12 +26,37 @@ module UsersHelper
   end
 
   def mutual_friends(user)
-    mutual = current_user.friends
-    k = []
-    mutual.each do |m|
-      r = m.friends.include?(user)
-      k << User.find(user.id)
-    end
-    return k
+    user.friends
+  end
+
+  def friend_request_sent?(user)
+    current_user.friend_sent.exists?(sent_to_id: user.id, status: false)
+  end
+
+  # Checks whether or not a user has sent a friend request to the current user
+  # returning either true or false
+  def friend_request_recieved?(user)
+    current_user.friend_request.exists?(sent_by_id: user.id, status: false)
+  end
+
+  # Checks whether or not a user has had a friend request sent to them by the current user
+  # or if the current user has been sent a friend request by the user
+  # returning either true or false
+  def possible_friend?(user)
+    request_sent = current_user.friend_sent.exists?(sent_to_id: user.id)
+    request_recieved = current_user.friend_request.exists?(sent_by_id: user.id)
+
+    return true if request_sent != request_recieved
+    return true if request_sent == request_recieved && request_sent == true
+    return false if request_sent == request_recieved && request_sent == false
+  end
+
+  def friends_already(user)
+    Friendship.find_by(sent_by_id: current_user.id, sent_to_id: user.id, status: true) || 
+    Friendship.find_by(sent_by_id: user.id, sent_to_id: current_user.id, status: true)
+  end
+
+  def me?(user)
+    user == current_user
   end
 end
